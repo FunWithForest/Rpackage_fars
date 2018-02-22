fa8e07c6f44db02520a75f992f41cd166b9dcd2d
FunWithForest <stefan.schmidt@axa.de>
2018-02-22 15:00:18 +0100

    minor changes in test

--- tests/test-function.R
+++ tests/test-function.R
@@ -1,4 +1,5 @@
 require(testthat)
 require(fars)
-expect_that(fars_map_state(1,2019), throws_error("file 'accident_2019.csv.bz2' does not existX"))
+#expect_that(fars_map_state(1,2019), throws_error("file 'accident_2019.csv.bz2' does not existX"))
+expect_that(fars_map_state(1,2019), throws_error("file '' does not existX"))
 
